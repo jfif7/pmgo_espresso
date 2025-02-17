aca4f0ac051401e278409b95523292868335f49f
jfif7 <github@onino.tw>
2025-02-18 04:14:33 +0800

    Implement output

--- espresso_logic/wasm_bridge.cpp
+++ espresso_logic/wasm_bridge.cpp
@@ -3,26 +3,27 @@
 #endif
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <sstream>
 #include "espresso.h"
 #include "cvrin_stream.hpp"
 
-int espresso_bridge(std::istream &is);
+char *espresso_bridge(std::istream &);
+const std::string get_solution(pPLA);
 
 extern "C" {
 #if __EMSCRIPTEN__
 EMSCRIPTEN_KEEPALIVE
 #endif
-const char *run_espresso(const char *input_char) {
-    std::string input(input_char);
+char *run_espresso(const char *input_char) {
     std::cout << "This is C++!\n" << std::endl;
-    std::istringstream is(input);
-    espresso_bridge(is);
-    return "what?";
+    std::istringstream is(input_char);
+    char *solution = espresso_bridge(is);
+    return solution;
 }
 }
 
-int espresso_bridge(std::istream &is) {
+char *espresso_bridge(std::istream &is) {
     int i, j, first, last, strategy, out_type, option;
     pPLA PLA;
     pcover F, Fold, Dold;
@@ -73,10 +74,8 @@ int espresso_bridge(std::istream &is) {
         free_cover(Fold);
     }
 
-    /* Output the solution */
-    // if (print_solution) {
-    //     EXECUTE(fprint_pla(stdout, PLA, out_type), WRITE_TIME, PLA->F, cost);
-    // }
+    const std::string &solution = get_solution(PLA);
+    std::cout << solution << std::endl;
 
     /* Crash and burn if there was a verify error */
     if (error) {
@@ -90,5 +89,38 @@ int espresso_bridge(std::istream &is) {
     sf_cleanup();   /* free unused set structures */
     sm_cleanup();   /* sparse matrix cleanup */
 
-    return 0;
+    char *cstr = new char[solution.size() + 1];
+    std::strcpy(cstr, solution.c_str());
+
+    return cstr;
+}
+
+const std::string get_solution(pPLA PLA) {
+    std::string solution;
+    pcube last, p;
+
+    foreach_set(PLA->F, last, p) {
+        for (int var = 0; var < cube.num_binary_vars; var++) {
+            solution.push_back("?01-"[GETINPUT(p, var)]);
+        }
+
+        for (int var = cube.num_binary_vars; var < cube.num_vars - 1; var++) {
+            solution.push_back(' ');
+            for (int i = cube.first_part[var]; i <= cube.last_part[var]; i++) {
+                solution.push_back("01"[is_in_set(p, i) != 0]);
+            }
+        }
+
+        if (cube.output != -1) {
+            int last_part = cube.last_part[cube.output];
+            solution.push_back(' ');
+
+            for (int i = cube.first_part[cube.output]; i <= last_part; i++) {
+                solution.push_back("01"[is_in_set(p, i) != 0]);
+            }
+        }
+
+        solution.push_back('\n');
+    }
+    return solution;
 }
